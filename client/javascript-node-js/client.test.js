ecb0e4e65684c58dcf7c7d68d15a5b85ab53e9ad
Overtorment <overtorment@gmail.com>
2017-11-15 06:36:05 +0200

    TST

--- client/javascript-node-js/client.test.js
+++ client/javascript-node-js/client.test.js
@@ -11,16 +11,14 @@ var iteration = 1
 var hub = noobhub.new({server: 'localhost', port: 1337})
 
 setTimeout(function () {
-  console.log('tests NOT ok')
-  process.exit()
+  console.log('tests NOT ok (timeout)')
+  process.exit(1)
 }, 5000)
 
 hub.subscribe({
   channel: 'testChannel' + require('crypto').createHash('md5').update(Math.random().toString()).digest('hex'),
   callback: function (data) {
-      // console.log('get data callback')
     assert.deepEqual(data, randomData)
-      // console.log('iteration', iteration, 'ok')
 
     if (iteration++ < 50) {
       randomData = {rand: Math.random()}
@@ -31,14 +29,12 @@ hub.subscribe({
     }
   },
   subscribedCallback: function (socket) {
-      // console.log('subscribed callback')
     hub.publish(randomData, function () {
-        // console.log('data sent')
     })
   },
   errorCallback: function (err) {
     console.log('error callback', err)
-    assert(false)
+    process.exit(1)
   }
 })
 
--- run-tests.sh
+++ run-tests.sh
@@ -9,8 +9,11 @@ if [ `netstat -ltnup 2>/dev/null | grep 1337 | wc -l` == 1 ]
 then
   echo running tests...
   nodejs client/javascript-node-js/client.test.js
+  EXT=$?
   echo killing Noobhub server...
   jobs -l | awk '{print $2}' | xargs kill -9
+  exit $EXT
 else
   echo Noobhub not started, tests didnt run
+  exit 1
 fi
